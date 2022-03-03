edb56cfe1dbf546113ba76117896a8c6a5fb4b71
unknown <csgimeno@up.edu.ph>
2022-03-03 19:49:39 +0800

    Lecture 1 Assignment

---
+++ Lecture1/Assignments/assignment_lec1_c2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void) {
+    int minuend = 87, subtrahend = 15, difference = 0;
+
+    difference = minuend - subtrahend;
+
+    printf("%d minus %d is equal to %d.", minuend, subtrahend, difference);
+
+    return 0;
+}
---
+++ Lecture1/Assignments/assignment_lec1_c4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(Void){                            //Added an opening brace
+    int sum;                               //Changed INT into int
+    //COMPUTE RESULT                         Changed into a one line comment
+    sum = 25 + 37 - 19;                    //Added semicolon
+    // DISPLAY RESULTS                       Changed into a one line comment and removed redundant slashes
+    printf ("The answer is %i\n",sum);     //Added a comma between string and variable
+    return 0;
+}
